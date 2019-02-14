e8ffdb389b6772aabc6e1b8ffd3dd77225d4bf3d
Lucas de Oliveira Silva <lucas.silva@dextra-sw.com>
2019-02-14 15:52:16 -0200

    Simple runner

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import time
 from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--start-maximized')
 
 driver = webdriver.Chrome('/home/lucas.silva/projects/ai-dino/chromedriver',
                           options=chrome_options)  # Optional argument, if not specified will search path.
-driver.get('https://chromedino.com/')
+driver.get('chrome://dino')
+
+actions = ActionChains(driver)
+actions.send_keys(Keys.SPACE)
+actions.perform()
+
+for _ in range(10 ** 6):
+    actions.send_keys(Keys.ARROW_UP)
+    actions.perform()
+    time.sleep(0.1)
+
 driver.quit()
