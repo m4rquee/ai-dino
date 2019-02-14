a1e87a090711c12a5f691b2fa9e924e7f56b46bb
Lucas de Oliveira Silva <lucas.silva@dextra-sw.com>
2019-02-14 16:51:35 -0200

    Getting game values

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import time
+from timeit import default_timer as timer
+
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
+
+
+def get_score():
+    return driver.execute_script('return Runner().distanceRan')
+
+
+def start_game(actions):
+    actions.send_keys(Keys.SPACE)
+    actions.perform()
+
+
+def crashed(driver):
+    return driver.execute_script('return Runner().crashed')
+
+
+def run_loop(driver, actions):
+    while not crashed(driver):
+        actions.send_keys(Keys.ARROW_UP)
+        actions.perform()
+        time.sleep(0.1)
+
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
@@ -11,13 +34,17 @@ driver = webdriver.Chrome('/home/lucas.silva/projects/ai-dino/chromedriver',
                           options=chrome_options)  # Optional argument, if not specified will search path.
 driver.get('chrome://dino')
 
-actions = ActionChains(driver)
-actions.send_keys(Keys.SPACE)
-actions.perform()
+print('Starting session!')
 
-for _ in range(10 ** 6):
-    actions.send_keys(Keys.ARROW_UP)
-    actions.perform()
-    time.sleep(0.1)
+actions = ActionChains(driver)
+
+for i in range(10):
+    print('Starting %ith game' % i)
+    start_game(actions)
+    start = timer()
+    run_loop(driver, actions)
+    print('Score:', timer() - start)
 
 driver.quit()
+
+print('Game session')
